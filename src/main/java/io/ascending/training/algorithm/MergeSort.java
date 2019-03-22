fd9cde1e48a83e88ae628d581d2de35582e30c83
ryo <ryo.hang@gmail.com>
2019-03-21 23:09:22 -0400

    add merge sort

---
+++ src/main/java/io/ascending/training/algorithm/MergeSort.java
@@ -0,0 +1,59 @@
+package io.ascending.training.algorithm;
+
+public class MergeSort {
+
+    public  void sort(int[] a, int n) {
+        if (n < 2) {
+            return;
+        }
+        int mid = n / 2;
+        int[] l = new int[mid];
+        int[] r = new int[n - mid];
+
+        for (int i = 0; i < mid; i++) {
+            l[i] = a[i];
+        }
+        for (int i = mid; i < n; i++) {
+            r[i - mid] = a[i];
+        }
+        sort(l, mid);
+        sort(r, n - mid);
+
+        merge(a, l, r, mid, n - mid);
+    }
+
+    public void merge(
+            int[] a, int[] l, int[] r, int left, int right) {
+
+        int i = 0, j = 0, k = 0;
+        while (i < left && j < right) {
+            if (l[i] <= r[j]) {
+                a[k++] = l[i++];
+            }
+            else {
+                a[k++] = r[j++];
+            }
+        }
+        while (i < left) {
+            a[k++] = l[i++];
+        }
+        while (j < right) {
+            a[k++] = r[j++];
+        }
+    }
+    public void printArray(int arr[])
+    {
+        int n = arr.length;
+        for (int i=0; i<n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+
+    public static void main(String[] args){
+        int[] inputs = {12, 11, 13, 5, 6, 7,8};
+        MergeSort ms = new MergeSort();
+        ms.sort(inputs,7);
+        ms.printArray(inputs);
+
+    }
+}
