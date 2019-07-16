5667b5e5989982173b67488c476c14afabfda0ca
ryo <ryo.hang@gmail.com>
2019-07-15 23:54:37 -0400

    update lambdaTest java

--- src/main/java/io/ascending/training/lambda/LambdaTest.java
+++ src/main/java/io/ascending/training/lambda/LambdaTest.java
@@ -8,22 +8,25 @@ public class LambdaTest {
         String myStringFunction(String str);
     }
 
-    class MyStringImpl implements MyString{
+    static class MyStringImpl implements MyString{
+        public MyStringImpl(){};
 
         @Override
         public String myStringFunction(String str) {
             return "Good Morning " + str + "!";
         }
     }
-
-
+    
     public static void main(String[] args){
+        MyString classString = new LambdaTest.MyStringImpl();
+        System.out.println(classString.myStringFunction("Ryo"));
+
+
         MyString value = (str) -> "Good Morning " + str + "!";
         MyString value2 = (str) -> "Good Afternoon " + str + "!";;
 
         System.out.println(value.myStringFunction("Luis"));
         System.out.println(value2.myStringFunction("Ryo"));
-
 
         List<String> items = new ArrayList<>();
         items.add("A");
@@ -34,8 +37,8 @@ public class LambdaTest {
         for(String item: items){
             System.out.println(item);
         }
-        items.forEach(abce->{
-            System.out.println(abce);
+        items.forEach((String x)->{
+            System.out.println(x);
         });
 
     }
